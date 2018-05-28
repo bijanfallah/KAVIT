25878f69a49e1b30d682d172708cb6110a01123d
Bijan Fallah <bijan.fallah@met.fu-berlin.de>
2018-05-28 10:48:16 +0200

    added new image feature extraction codes

---
+++ codes/image_features_train_7_2_batch_1.py
@@ -0,0 +1,84 @@
+'''
+Program to extract the image features using the Keras 
+pretrained models:
+https://www.kaggle.com/lemonkoala/pretrained-keras-models-symlinked-not-copied
+for the quality of the features 
+
+'''
+file_name='/media/fallah/ext01/train_figs/train_1_features_2'
+images_path = '/media/fallah/ext01/train_figs/train_1/'
+import matplotlib.pyplot as plt
+import numpy             as np
+import os
+from keras.preprocessing               import image
+from keras.applications.resnet50       import ResNet50, preprocess_input
+from keras.applications.imagenet_utils import decode_predictions
+# activate the next 9 lines only once:
+inputs_dir = "/media/fallah/ext01/train_figs/"
+models_dir = os.path.expanduser(os.path.join("~", ".keras", "models"))
+#os.makedirs(models_dir)
+for file in os.listdir(inputs_dir):
+    if file.endswith(".json") or file.endswith(".h5"):
+        os.symlink(
+            os.path.join(inputs_dir, file),
+            os.path.join(models_dir, file)
+        )
+#import os
+#import numpy as np
+import pandas as pd
+from keras.preprocessing import image
+import keras.applications.resnet50 as resnet50
+import keras.applications.xception as xception
+import keras.applications.inception_v3 as inception_v3
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+# read the train data:
+
+imgs = os.listdir(images_path)
+features = pd.DataFrame()
+features['image'] = imgs
+# ---------------------------------------------------
+
+
+
+
+sns.set_style("whitegrid")
+sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+
+#%matplotlib inline
+#the model is inception V3
+inception_model = inception_v3.InceptionV3(weights='imagenet')
+def image_classify(model, pak, img, top_n=3):
+    """Classify image and return top matches."""
+    target_size = (224, 224)
+    if img.size != target_size:
+        img = img.resize(target_size)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = pak.preprocess_input(x)
+    preds = model.predict(x)
+    return pak.decode_predictions(preds, top=top_n)[0]
+    
+images_dir = images_path
+image_files = [x.path for x in os.scandir(images_dir)]
+def incept_class(incept):
+    if incept[0][2] > .8 :
+        return 1
+    if (incept[0][2] < .8) &  (incept[0][2] > .5):
+        return 2
+    if (incept[0][2] < .5):
+        return 3
+        
+# loop over all images:
+class_qual = []
+for i in range(0,len(image_files)):
+#for i in range(0,100):
+    img = Image.open(image_files[i])
+    inception_preds = image_classify(inception_model, inception_v3, img)
+#    print(incept_class(inception_preds))
+    class_qual.extend([incept_class(inception_preds)])
+#print(class_qual)    
+features['class_qual'] =  class_qual
+features.to_pickle(file_name)      
+
